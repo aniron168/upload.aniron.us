e3880e6a90ae6082858df8d6504d6eda3cd6f4ed
ByteBard <129457446+aniron168@users.noreply.github.com>
2023-05-22 22:45:20 -0400

    more changes

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,229 @@ if (!check_logged_in() || !check_user_is_admin()) {
     exit;
 }
 
-// Rest of your admin dashboard code. This might include a list of users, a list of files, etc.
+// Rest of your admin dashboard code. This might include a list of files, users, etc.
+// Assuming you have a 'files' table and 'users' table in your database with appropriate columns
+$stmt = $conn->prepare("SELECT * FROM files");
+$stmt->execute();
+$files = $stmt->get_result();
+
+$stmt2 = $conn->prepare("SELECT * FROM users");
+$stmt2->execute();
+$users = $stmt2->get_result();
+
+
+$action = isset($_GET['action']) ? $_GET['action'] : '';
+
+//...
+
+switch ($action) {
+    case 'create_user':
+        // handle form submission for creating user
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+            $password = $_POST['password'];
+            create_user($email, $password);
+            header('Location: ' . SITE_URL . '/admin/index.php');
+            exit;
+        } else {
+            ?>
+            <form method="POST">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+    
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+    
+                <input type="submit" value="Create User">
+            </form>
+            <?php
+        }
+        break;
+    case 'change_password':
+        // handle form submission for changing user password
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_NUMBER_INT);
+            $password = $_POST['password'];
+            change_user_password($id, $password);
+            header('Location: ' . SITE_URL . '/admin/index.php');
+            exit;
+        } else {
+            ?>
+            <form method="POST">
+                <label for="user_id">User ID:</label>
+                <input type="number" id="user_id" name="user_id" required>
+    
+                <label for="password">New Password:</label>
+                <input type="password" id="password" name="password" required>
+    
+                <input type="submit" value="Change Password">
+            </form>
+            <?php
+        }
+        break;
+    // Similarly, for the rest of the cases, add forms for inputting necessary information. 
+    //...
+    case 'delete_user':
+        // handle user deletion
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_NUMBER_INT);
+            delete_user($id);
+            header('Location: ' . SITE_URL . '/admin/index.php');
+            exit;
+        } else {
+            ?>
+            <form method="POST">
+                <label for="user_id">User ID:</label>
+                <input type="number" id="user_id" name="user_id" required>
+    
+                <input type="submit" value="Delete User">
+            </form>
+            <?php
+        }
+        break;
+    case 'make_admin':
+        // handle form submission for making user admin
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_NUMBER_INT);
+            assign_admin_privilege($id);
+            header('Location: ' . SITE_URL . '/admin/index.php');
+            exit;
+        } else {
+            ?>
+            <form method="POST">
+                <label for="user_id">User ID:</label>
+                <input type="number" id="user_id" name="user_id" required>
+    
+                <input type="submit" value="Assign Admin">
+            </form>
+            <?php
+        }
+        break;
+        
+    case 'upload_file':
+        // handle form submission for uploading file
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Ensure a file was uploaded
+            if (isset($_FILES['file']) && $_FILES['file']['error'] == 0) {
+                $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+                $uploaded_by = $_SESSION['user_id'];
+    
+                // Define your upload directory. Make sure this is writeable by your server.
+                $uploadDir = '/path/to/your/uploads/directory';
+    
+                // Create a unique name for the file to prevent overwriting existing files
+                $fileName = uniqid() . '_' . basename($_FILES['file']['name']);
+                $uploadFilePath = $uploadDir . '/' . $fileName;
+    
+                // Move the file to your upload directory. Check if the move was successful.
+                if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadFilePath)) {
+                    upload_file($name, $uploadFilePath, $uploaded_by);
+    
+                    header('Location: ' . SITE_URL . '/admin/index.php');
+                    exit;
+                } else {
+                    // Error handling
+                    echo "An error occurred while moving the uploaded file.";
+                }
+            } else {
+                // Error handling. In a real application, you should provide more detailed error messages
+                echo "An error occurred while uploading the file.";
+            }
+        } else {
+            ?>
+            <form method="POST" enctype="multipart/form-data">
+                <label for="name">File Name:</label>
+                <input type="text" id="name" name="name" required>
+    
+                <label for="file">File:</label>
+                <input type="file" id="file" name="file" required>
+        
+                <input type="submit" value="Upload File">
+            </form>
+            <?php
+        }
+        break;
+    case 'delete_file':
+        // handle file deletion
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = filter_input(INPUT_POST, 'file_id', FILTER_SANITIZE_NUMBER_INT);
+            delete_file($id);
+            header('Location: ' . SITE_URL . '/admin/index.php');
+            exit;
+        } else {
+            ?>
+            <form method="POST">
+                <label for="file_id">File ID:</label>
+                <input type="number" id="file_id" name="file_id" required>
+    
+                <input type="submit" value="Delete File">
+            </form>
+            <?php
+        }
+        break;
+    default:
+        // show dashboard
+}
+
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+</head>
+<body>
+    <h1>Welcome, Admin!</h1>
 
+    <h2>All Files</h2>
+    <?php if ($files->num_rows > 0): ?>
+        <ul>
+            <!-- In file listing -->
+            <?php while ($file = $files->fetch_assoc()): ?>
+                <li>
+                    <a href="/files/<?php echo htmlspecialchars($file['filename']); ?>"><?php echo htmlspecialchars($file['filename']); ?></a>
+                    - Uploaded by <?php echo htmlspecialchars($file['uploaded_by']); ?>
+                    <a href="index.php?action=delete_file&file_id=<?php echo $file['id']; ?>">Delete File</a>
+                    
+                    <!-- Hidden input form -->
+                    <form action="index.php?action=delete_file" method="POST" id="delete_file_<?php echo $file['id']; ?>" style="display: none;">
+                        <input type="hidden" name="file_id" value="<?php echo $file['id']; ?>">
+                    </form>
+                </li>
+            <?php endwhile; ?>
+
+            <!-- In user listing -->
+            <?php while ($user = $users->fetch_assoc()): ?>
+                <li>
+                    <?php echo htmlspecialchars($user['email']); ?>
+                    <a href="index.php?action=change_password&user_id=<?php echo $user['id']; ?>">Change User Password</a>
+                    <a href="index.php?action=delete_user&user_id=<?php echo $user['id']; ?>">Delete User</a>
+                    <a href="index.php?action=make_admin&user_id=<?php echo $user['id']; ?>">Assign Admin</a>
+
+                    <!-- Hidden input forms -->
+                    <form action="index.php?action=change_password" method="POST" id="change_password_<?php echo $user['id']; ?>" style="display: none;">
+                        <input type="hidden" name="user_id" value="<?php echo $user['id']; ?>">
+                    </form>
+                    <form action="index.php?action=delete_user" method="POST" id="delete_user_<?php echo $user['id']; ?>" style="display: none;">
+                        <input type="hidden" name="user_id" value="<?php echo $user['id']; ?>">
+                    </form>
+                    <form action="index.php?action=make_admin" method="POST" id="make_admin_<?php echo $user['id']; ?>" style="display: none;">
+                        <input type="hidden" name="user_id" value="<?php echo $user['id']; ?>">
+                    </form>
+                </li>
+            <?php endwhile; ?>
+        </ul>
+    <?php else: ?>
+        <p>No users have been registered.</p>
+    <?php endif; ?>
+
+    <a href="index.php?action=create_user">Create User</a>
+    <a href="index.php?action=change_password">Change User Password</a>
+    <!-- ...and so on -->
+
+    <!-- Logout button -->
+    <form action="/logout.php" method="POST">
+        <button type="submit">Logout</button>
+    </form>
+</body>
+</html>
--- includes/functions.php
+++ includes/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('config.php'); 
+require_once('db.php'); 
 session_start();
 
 /**
@@ -26,13 +27,87 @@ function display_error($error_message) {
 
 function check_user_is_admin() {
     // You'll need to implement this function based on your users table.
-    // It should return true if the currently logged in user is an admin, and false otherwise.
+    // It should return true if the currently logged in user is an admin, and false otherwise.function check_user_is_admin() {
+    // Ensure the user is logged in before checking if they're an admin
+
+    if (!isset($_SESSION['user_id'])) {
+        return false;
+    }
+
+    // Prepare a SQL statement
+    $stmt = $GLOBALS['conn']->prepare("SELECT is_admin FROM Users WHERE id = ?");
+    $stmt->bind_param('i', $_SESSION['user_id']);
+
+    // Execute the SQL statement
+    $stmt->execute();
+
+    // Bind the result to a variable
+    $stmt->bind_result($is_admin);
+
+    // Fetch the result. This will populate $is_admin with the value from the database
+    $stmt->fetch();
+
+    // Close the statement
+    $stmt->close();
+
+    // If $is_admin is true (1), return true. Otherwise, return false
+    return $is_admin == 1;
 }
 
 function handle_error($message, $redirect_url = SITE_URL) {
     $_SESSION['error_message'] = $message;
     header('Location: ' . $redirect_url);
     exit;
+}
+
+function create_user($email, $password) {
+    global $db;
+    $hashed_password = hash_password($password);
+    $sql = "INSERT INTO Users (email, password) VALUES (?, ?)";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("ss", $email, $hashed_password);
+    $stmt->execute();
+}
+
+function delete_user($id) {
+    global $db;
+    $sql = "DELETE FROM Users WHERE id = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+}
+
+function change_user_password($id, $new_password) {
+    global $db;
+    $hashed_password = hash_password($new_password);
+    $sql = "UPDATE Users SET password = ? WHERE id = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("si", $hashed_password, $id);
+    $stmt->execute();
+}
+
+function assign_admin_privilege($id) {
+    global $db;
+    $sql = "UPDATE Users SET is_admin = 1 WHERE id = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+}
+
+function upload_file($name, $path, $uploaded_by) {
+    global $db;
+    $sql = "INSERT INTO Files (name, path, uploaded_by) VALUES (?, ?, ?)";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("ssi", $name, $path, $uploaded_by);
+    $stmt->execute();
+}
+
+function delete_file($id) {
+    global $db;
+    $sql = "DELETE FROM Files WHERE id = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
 }
 
 ?>
